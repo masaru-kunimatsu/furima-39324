be1c078250b8cf1f601cc78fb6bc08c88c030c62
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-02 21:36:46 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name_phonetic, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :first_name_phonetic, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :birth_day, presence: true
