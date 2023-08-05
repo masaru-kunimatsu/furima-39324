f7cbbeb0f805eadbc9a04d7ff436712f1badbb42
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-06 06:31:01 +0900

    java導入

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addTaxDomPrice = Math.floor(inputValue * 0.1)
+    addTaxDom.innerHTML = addTaxDomPrice
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue - addTaxDomPrice)
+  })
 });
