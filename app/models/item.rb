7ca9cb3e770424fd09738b47d03ed4a4227e5e55
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-08 06:03:42 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
   belongs_to :user
-  has_one :purchase_record
+  #has_one :purchase_record
   has_one_attached :image
 
   validates :product_name, presence: true
@@ -16,7 +16,6 @@ class Item < ApplicationRecord
   validates :shipping_cost_burden_id, presence: true, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 3, message: "can't be blank"} 
   validates :prefecture_id, presence: true, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 48, message: "can't be blank"} 
   validates :shipping_day_id, presence: true, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 4, message: "can't be blank"} 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/}
-  validates :user, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true
 end
