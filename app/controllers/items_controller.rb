d32023525d18263a3ef83dc7c1269dbd882c1e05
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-09 18:30:01 +0900

    orderに修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all
+    @items = Item.order
   end
   
   def new
