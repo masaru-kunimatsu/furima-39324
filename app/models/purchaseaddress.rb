e5d4e33eadb986465ced9b9eeed5789d7f3f71f6
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-17 06:34:15 +0900

    purchaseaddressの記述

--- app/models/purchaseaddress.rb
+++ app/models/purchaseaddress.rb
@@ -4,15 +4,15 @@ class PurchaseAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :block_number, :building, :telephone_number, :purchase_record
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
-    validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 48, message: "can't be blank"} 
+    validates :city 
+    validates :block_number
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/}
   end
 
-
-  # ここにバリデーションの処理を書く
-
   def save
-    # 各テーブルにデータを保存する処理を書く
+    purchase_record = Purchase_record.create(user_id: user_id, item_id: item_id)
+    Shipping_adress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block_number: block_number, building_name: building_name, telephone_number: telephone_number, purchase_record_id: purchase_record.id)
   end
 end
