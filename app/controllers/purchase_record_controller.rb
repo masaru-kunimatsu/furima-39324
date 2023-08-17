19c5719363bd586e723054583b814a2e4a9277c2
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-18 05:29:26 +0900

    controllerとview作成

--- app/controllers/purchase_record_controller.rb
+++ app/controllers/purchase_record_controller.rb
@@ -1,2 +1,26 @@
 class PurchaseRecordController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @purchase_address = Purchaseaddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture, :city, :block_number, :building_name, :telephone_number, :purchase_record_id, :item_id).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
     resources :items
-  resources :purchase_record, only: [:index, :create] do
+  resources :purchase_record, only: [:index, :create, :new] do
     resources :shipping_adress, only: :create
   end
 end
