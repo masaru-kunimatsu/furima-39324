6e3a9615c56c3c15f46da58238685f1105164193
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-18 05:00:54 +0900

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
     resources :items
+  resources :purchase_record, only: [:index, :create] do
+    resources :shipping_adress, only: :create
+  end
 end
