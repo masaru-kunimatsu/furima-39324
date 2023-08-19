52298efa01fc6f44fee3a030d9826a58cc5cea78
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-19 22:11:00 +0900

    クレカ機能以外完了

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,28 +1,25 @@
-class PurchaseRecordController < ApplicationController
+class PurchaseRecordsController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
-  def new
-    @purchase_address = PurchaseAddress.new
-  end
-
   def create
+    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture, :city, :block_number, :building_name, :telephone_number, :purchase_record_id, :item_id).merge(user_id: current_user.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :block_number, :building, :telephone_number, :purchase_record_id ).merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -14,7 +14,7 @@ class PurchaseAddress
   end
 
   def save
-    purchase_record = Purchase_record.create(user_id: user_id, item_id: item_id)
-    Shipping_adress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block_number: block_number, building_name: building_name, telephone_number: telephone_number, purchase_record_id: purchase_record.id)
+    @purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    ShippingAdress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block_number: block_number, building: building, telephone_number: telephone_number, purchase_record_id: purchase_record.id)
   end
 end
