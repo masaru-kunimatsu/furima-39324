4a98c41df97af6aefe2cd857979af2ee2b2b05ba
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-08-21 00:03:53 +0900

    購入機能ほぼ完成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -9,17 +9,27 @@ class PurchaseRecordsController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
-      render :index
+      render template: 'purchase_records/index'
     end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :block_number, :building, :telephone_number, :purchase_record_id ).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :block_number, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_6db629ada0a57d5f085821b9"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
